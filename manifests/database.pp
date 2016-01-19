e286ee0270cca03375bc3600d87f16daff85a604
Eric Zounes <eric.zounes@puppetlabs.com>
2016-01-19 13:39:11 -0800

    Make the db class more generic.

--- manifests/database.pp
+++ manifests/database.pp
@@ -20,43 +20,44 @@ class icinga2::database(
   }
   validate_absolute_path($db_schema)
 
-  if $::icinga2::db_type == 'mysql' {
-
-    package { 'icinga2-ido-mysql':
-      ensure   => $::icinga2::package_ensure,
-    }
-
-    # TODO: is there a better way?
-    Package['icinga2-ido-mysql'] ->
-    exec { 'mysql_schema_load':
-      user    => 'root',
-      path    => $::path,
-      command => "mysql -h '${::icinga2::db_host}' -u '${::icinga2::db_user}' -p'${::icinga2::db_pass}' '${::icinga2::db_name}' < '${db_schema}' && touch /etc/icinga2/mysql_schema_loaded.txt",
-      creates => '/etc/icinga2/mysql_schema_loaded.txt',
-    }
+  $ido_package = $::icinga2::db_type ? {
+    'mysql' => 'icinga2-ido-mysql',
+    'pgsql' => 'icinga2-ido-pgsql',
   }
-  elsif $::icinga2::db_type == 'pgsql' {
 
-    package { 'icinga2-ido-pgsql':
-      ensure   => $::icinga2::package_ensure,
+  package { $ido_package:
+    ensure => $::icinga2::package_ensure
+  }
+
+  if $manage_schema {
+    if $::icinga2::db_type == 'mysql' {
+      # TODO: is there a better way?
+      Package['icinga2-ido-mysql'] ->
+      exec { 'mysql_schema_load':
+        user    => 'root',
+        path    => $::path,
+        command => "mysql -h '${::icinga2::db_host}' -u '${::icinga2::db_user}' -p'${::icinga2::db_pass}' '${::icinga2::db_name}' < '${db_schema}' && touch /etc/icinga2/mysql_schema_loaded.txt",
+        creates => '/etc/icinga2/mysql_schema_loaded.txt',
+      }
     }
+    elsif $::icinga2::db_type == 'pgsql' {
+      # TODO: is there a better way?
+      if $::icinga2::db_port {
+        $port = "-p ${::icinga2::db_port}"
+      } else {
+        $port = undef
+      }
 
-    # TODO: is there a better way?
-    if $::icinga2::db_port {
-      $port = "-p ${::icinga2::db_port}"
-    } else {
-      $port = undef
-    }
-
-    Package['icinga2-ido-pgsql'] ->
-    exec { 'postgres_schema_load':
-      user        => 'root',
-      path        => $::path,
-      environment => [
-        "PGPASSWORD=${::icinga2::db_pass}",
-      ],
-      command     => "psql -U '${::icinga2::db_user}' -h '${::icinga2::db_host}' ${port} -d '${::icinga2::db_name}' < '${db_schema}' && touch /etc/icinga2/postgres_schema_loaded.txt",
-      creates     => '/etc/icinga2/postgres_schema_loaded.txt',
+      Package['icinga2-ido-pgsql'] ->
+      exec { 'postgres_schema_load':
+        user        => 'root',
+        path        => $::path,
+        environment => [
+          "PGPASSWORD=${::icinga2::db_pass}",
+        ],
+        command     => "psql -U '${::icinga2::db_user}' -h '${::icinga2::db_host}' ${port} -d '${::icinga2::db_name}' < '${db_schema}' && touch /etc/icinga2/postgres_schema_loaded.txt",
+        creates     => '/etc/icinga2/postgres_schema_loaded.txt',
+      }
     }
   }
 
