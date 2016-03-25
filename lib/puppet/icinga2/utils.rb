0f7e16936ed0ef64ecf26fdced66cb561c569222
Eric Zounes <eric.zounes@puppetlabs.com>
2016-03-25 15:28:23 -0700

    Fix the config_values functions.

--- lib/puppet/icinga2/utils.rb
+++ lib/puppet/icinga2/utils.rb
@@ -20,7 +20,7 @@ module Puppet
               return value.to_f
             else
               # remove quotes from oldstyle values "something"
-              value = value.gsub(/(^"|"$)/, '')
+              value = value.gsub(/^"(.*)"$/, '\1')
             end
           end
           value.to_s.dump
